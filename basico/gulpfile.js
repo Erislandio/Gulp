bfb7d75a60c1b1baef8362a4070d4b29bdc27bdd
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-07 22:42:50 -0200

    Task basica

--- basico/gulpfile.js
+++ basico/gulpfile.js
@@ -1,3 +1,27 @@
 const gulp = require('gulp');
 
 // Task
+
+gulp.task('default', () => {
+    gulp.start('copiar', 'fim')
+    // console.log('ok')
+})
+
+// ! task de pre requisito
+gulp.task('copiar', ['antes1', 'antes2'], () => {
+    console.log('copiar')
+})
+
+gulp.task('antes1', () => {
+    console.log('antes1')
+})
+
+
+gulp.task('antes2', () => {
+    console.log('antes2')
+})
+
+
+gulp.task('fim', () => {
+    console.log('fim')
+})
