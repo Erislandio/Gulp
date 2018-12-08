6cbf2479c74b8e8d95e5ab7c726bcb1b83c4d44f
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-08 12:18:31 -0200

    Ajustes nas task de html e css

--- spa/GulpTasks/app.js
+++ spa/GulpTasks/app.js
@@ -1,26 +1,36 @@
 // ! inicializador  estrutura basica
 
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
+
 
 gulp.task('app', ['app.html', 'app.css', 'app.js', 'app.imgs'])
 
 
 gulp.task('app.html', () => {
-
+    return gulp.src('src/**/*.html')
+        // remove espacos em branco
+        .pipe(htmlmin({ conservativeCollapse: true }))
+        .pipe(gulp.dest('build'))
 })
 
 
 gulp.task('app.css', () => {
-    
+
 })
 
 
 
 gulp.task('app.js', () => {
-    
+
 })
 
 
 gulp.task('app.imgs', () => {
-    
+
 })
